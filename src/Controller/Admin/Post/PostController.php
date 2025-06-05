39d09f06d78f0bf5bbe6cb7f3aed4e5ea35a827a
ahmed.b <baazizahmed@gmail.com>
2025-06-05 23:31:06 +0200

    feat: L'admin peut maintenant gérer ses articles(CRUD)

--- src/Controller/Admin/Post/PostController.php
+++ src/Controller/Admin/Post/PostController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin\Post;
 use App\Entity\Post;
 use App\Entity\User;
 use App\Form\AdminPostForm;
+use App\Repository\PostRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,13 +17,18 @@ final class PostController extends AbstractController
 {
     public function __construct(
         private EntityManagerInterface $entityManager,
+        private PostRepository $postReposit,
     ) {
     }
 
     #[Route('/post/index', name: 'app_admin_post_index', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('pages/admin/post/index.html.twig');
+        $posts = $this->postReposit->findAll();
+
+        return $this->render('pages/admin/post/index.html.twig', [
+            'posts' => $posts,
+        ]);
     }
 
     #[Route('/post/create', name: 'app_admin_post_create', methods: ['GET', 'POST'])]
@@ -47,7 +53,7 @@ final class PostController extends AbstractController
             $this->entityManager->persist($post);
             $this->entityManager->flush();
 
-            $this->addFlash('success', "L'article a été modifié");
+            $this->addFlash('success', "L'article a été créé");
 
             return $this->redirectToRoute('app_admin_post_index');
         }
@@ -55,5 +61,82 @@ final class PostController extends AbstractController
         return $this->render('pages/admin/post/create.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    #[Route('/post/show/{id<\d+>}', name: 'app_admin_post_show', methods: ['GET'])]
+    public function show(Post $post): Response
+    {
+        return $this->render('pages/admin/post/show.html.twig', [
+            'post' => $post,
+        ]);
+    }
+
+    #[Route('/post/edit/{id<\d+>}', name: 'app_admin_post_edit', methods: ['GET', 'POST'])]
+    public function edit(Post $post, Request $request): Response
+    {
+        $form = $this->createForm(AdminPostForm::class, $post);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /**
+             * @var User
+             */
+            $user = $this->getUser();
+
+            $post->setUser($user);
+            $post->setCreatedAt(new \DateTimeImmutable());
+            $post->setUpdatedAt(new \DateTimeImmutable());
+
+            $this->entityManager->persist($post);
+            $this->entityManager->flush();
+
+            $this->addFlash('success', "L'article a été modifié");
+
+            return $this->redirectToRoute('app_admin_post_index');
+        }
+
+        return $this->render('pages/admin/post/edit.html.twig', [
+            'post' => $post,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/post/delete/{id<\d+>}', name: 'app_admin_post_delete', methods: ['POST'])]
+    public function delete(Post $post, Request $request): Response
+    {
+        if ($this->isCsrfTokenValid("delete-post-{$post->getId()}", $request->request->get('csrf_token'))) {
+            $this->entityManager->remove($post);
+            $this->entityManager->flush();
+
+            $this->addFlash('success', "L'article a été suprimé");
+        }
+
+        return $this->redirectToRoute('app_admin_post_index');
+    }
+
+    #[Route('/post/publish/{id<\d+>}', name: 'app_admin_post_publish', methods: ['POST'])]
+    public function publish(Post $post, Request $request): Response
+    {
+        if (!$this->isCsrfTokenValid("publish-post-{$post->getId()}", $request->request->get('csrf_token'))) {
+            return $this->redirectToRoute('app_admin_post_index');
+        }
+
+        if ($post->isPublished()) {
+            $post->setIsPublished(false);
+            $post->setPublishedAt(null);
+
+            $this->addFlash('success', 'cet article a été retiré de la liste des publication');
+        } else {
+            $post->setIsPublished(true);
+            $post->setPublishedAt(new \DateTimeImmutable());
+
+            $this->addFlash('success', 'cet article a été publié');
+        }
+
+        $this->entityManager->persist($post);
+        $this->entityManager->flush();
+
+        return $this->redirectToRoute('app_admin_post_index');
     }
 }
